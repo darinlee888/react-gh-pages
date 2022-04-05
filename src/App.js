b7fde2cce10be7d23fb441fa809cd60a7e17548e
unknown <darinlee888@gmail.com>
2022-04-05 16:42:24 -0700

    Created base website

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1> React Posts Share</h1>
-      </header>
-    </div>
+    <main>
+      <h1>Darin Lee</h1>
+      <div id="contactInfo">
+        <a href = "mailto: darinlee888@gmail.com">darinlee888@gmail.com</a>
+        <a href="https://www.linkedin.com/in/darin-lee-3486031b1/" target="_blank">LinkedIn</a>
+        <div id="phone">Phone #: (415) 928-9213</div>
+      </div>
+      <div id="resume">
+        <button>High School</button>
+        <button>Undergraduate</button>
+        <button>Internship</button>
+        <button>Projects</button>
+        <button>Skills</button>
+      </div>
+    </main>
   );
 }
 
