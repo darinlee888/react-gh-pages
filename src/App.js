0bf2521870565ce349ad7b59c95bec029dbf7cb5
unknown <darinlee888@gmail.com>
2022-04-09 22:00:28 -0700

    Added GitHub profile and minor button changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -7,12 +6,13 @@ function App() {
       <h1>Darin Lee</h1>
       <div id="contactInfo">
         <a href = "mailto: darinlee888@gmail.com">darinlee888@gmail.com</a>
+        <a href="https://github.com/darinlee888" target="_blank">GitHub</a>
         <a href="https://www.linkedin.com/in/darin-lee-3486031b1/" target="_blank">LinkedIn</a>
-        <div id="phone">Phone #: (415) 928-9213</div>
+        <div id="phone">Phone: (415) 928-9213</div>
       </div>
       <div id="resume">
         <button>High School</button>
-        <button>Undergraduate</button>
+        <button>College</button>
         <button>Internship</button>
         <button>Projects</button>
         <button>Skills</button>
